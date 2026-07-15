a11f63ba85b80267e798bcc8003c6b73acbe0173
julianfumarola111710 <julianfumarola111710@gmail.com>
2026-07-14 21:07:50 -0500

    dashboard component

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
 import { AuthService } from '../../core/services/auth.service';
 import { TokenService } from '../../core/services/token.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -20,18 +24,14 @@ export class DashboardComponent {
   logout(): void {
     this.authService.logout().subscribe({
       next: () => {
-        /* Backend confirmó — ahora sí limpiamos y redirigimos */
         this.tokenService.clearSession();
         this.router.navigate(['/login']);
-        console.log('LOGOUT EXITOSO');
       },
       error: () => {
-        /* Si el backend falla, igual limpiamos localmente
-           para no dejar al usuario atrapado */
         this.tokenService.clearSession();
         this.router.navigate(['/login']);
-        console.error('LOGOUT con error de backend — sesión limpiada localmente');
       }
     });
   }
+
 }
