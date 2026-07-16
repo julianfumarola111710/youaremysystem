4bef00e1ac24e147c7eaf064bafacc3a0fc64cd5
jessibel02 <jessibel91@gmail.com>
2026-07-15 21:40:42 -0500

    Se agrega la ruta del modulo de productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ClientesComponent } from './features/clientes/clientes.component';
 import { authGuard } from './core/guards/auth.guard';
 import { NotifiComponent } from './features/notificaciones/notifi.component';
 import { UsuarioComponent } from './features/usuarios/usuario.component';
+import { ProductosComponent } from './features/productos/productos.component';
 
 export const routes: Routes = [
   {
@@ -34,6 +35,11 @@ export const routes: Routes = [
 {
   path: 'usuarios',
   component: UsuarioComponent,
+  canActivate: [authGuard]
+},
+{
+  path: 'productos',
+  component: ProductosComponent,
   canActivate: [authGuard]
 }
 ];
